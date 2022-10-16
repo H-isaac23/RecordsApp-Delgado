5884bf96bd9c5889bb80c2a345ef43d3c7364c00
Holo-23 <dev.isaac23@gmail.com>
2022-10-16 12:29:16 +0800

    finish video 13

--- office.php
+++ office.php
@@ -85,6 +85,7 @@
                                                 <th>City</th>
                                                 <th>Country</th>
                                                 <th>Postal</th>
+                                                <th>Action</th>
                                             </thead>
                                             <tbody>
                                                 <?php
@@ -98,6 +99,11 @@
                                                     <td><?php echo $office['city']; ?></td>
                                                     <td><?php echo $office['country']; ?></td>
                                                     <td><?php echo $office['postal']; ?></td>
+                                                    <td>
+                                                        <a href="/office-edit.php?id=<?php echo $office['id']; ?>">
+                                                            <button type="submit" class="btn btn-warning btn-fill pull-right">Edit</button>
+                                                        </a>
+                                                    </td>
                                                 </tr>
                                                 <?php endforeach ?>
                                             </tbody>
