6a45e50b73c005fb560e30685d1347b471842878
Holo-23 <dev.isaac23@gmail.com>
2022-10-16 11:43:18 +0800

    finish video 10

--- office.php
+++ office.php
@@ -24,7 +24,23 @@
         require('config/config.php');
         require('config/db.php');
 
-        $query = 'SELECT * FROM office ORDER BY name';
+        $results_per_page = 10;
+
+        $query = "SELECT * FROM office";
+        $result = mysqli_query($conn, $query);
+        $number_of_result = mysqli_num_rows($result);
+
+        $number_of_page = ceil($number_of_result/$results_per_page);
+
+        if(!isset($_GET['page'])) {
+            $page = 1;
+        } else {
+            $page = $_GET['page'];
+        }
+
+        $page_first_result = ($page-1) * $results_per_page;
+
+        $query = 'SELECT * FROM office ORDER BY name LIMIT '. $page_first_result . ', '. $results_per_page;
 
         $result = mysqli_query($conn, $query);
         $offices = mysqli_fetch_all($result, MYSQLI_ASSOC);
@@ -90,6 +106,11 @@
                                 </div>
                             </div>
                     </div>
+                    <?php
+                        for($page=1; $page <= $number_of_page; $page++) {
+                            echo '<a href = "office.php?page='. $page . '">' . $page . '</a>';
+                        }
+                    ?>
                 </div>
             </div>
             <footer class="footer">
